99d131c4558c133ce53f9bf26cab72b5b2b5cd4e
Linus Willner <hello@linuswillner.me>
2021-02-04 02:05:29 +0200

    Add material classes

---
+++ src/main/java/com/assemblyrobot/system/core/Material.java
@@ -0,0 +1,29 @@
+package com.assemblyrobot.system.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+public abstract class Material {
+  private static int nextFreeId = 1;
+
+  @Getter private final int id;
+  @Getter @Setter private long queueStartTime = 0;
+  @Getter @Setter private long queueEndTime = 0;
+  @Getter @Setter private long processingStartTime = 0;
+  @Getter @Setter private long processingEndTime = 0;
+
+  public Material() {
+    id = nextFreeId;
+    nextFreeId++;
+  }
+
+  public long getQueueDuration() {
+    return queueEndTime - queueStartTime;
+  }
+
+  public long getProcessingDuration() {
+    return processingEndTime - processingStartTime;
+  }
+}
