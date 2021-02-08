27abef510118ea822e499add4e5def3ca05b84b2
Linus Willner <18148938+linuswillner@users.noreply.github.com>
2021-02-08 14:56:34 +0200

    Make some refactors (#9)

--- src/main/java/com/assemblyrobot/simulator/core/clock/TickAdvanceListener.java
+++ src/main/java/com/assemblyrobot/simulator/core/clock/TickAdvanceListener.java
@@ -1,9 +1,8 @@
 package com.assemblyrobot.simulator.core.clock;
 
 public abstract class TickAdvanceListener {
-  protected TickAdvanceListener(Object o) {
-    // This constructor does nothing, it's just here to ensure that all TickAdvanceListeners define
-    // a constructor (which is where they attach themselves as tick advance listeners)
+  protected TickAdvanceListener() {
+    Clock.getInstance().registerTickAdvanceListener(this);
   }
 
   protected abstract void onTickAdvance(long ticksAdvanced);
