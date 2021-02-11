f20c60e0937e864f426a0023d12620551c96c2b5
Linus Willner <hello@linuswillner.me>
2021-02-11 17:24:35 +0200

    Document TickAdvanceListener

--- src/main/java/com/assemblyrobot/simulator/core/clock/TickAdvanceListener.java
+++ src/main/java/com/assemblyrobot/simulator/core/clock/TickAdvanceListener.java
@@ -1,11 +1,21 @@
 package com.assemblyrobot.simulator.core.clock;
 
+/**
+ * Generic tick advancement listener. Any class that extends this class will automatically be
+ * registered as a tick advancement listener in the {@link Clock}.
+ */
 public abstract class TickAdvanceListener {
   protected TickAdvanceListener() {
     Clock.getInstance().registerTickAdvanceListener(this);
   }
 
+  /**
+   * Callback method for when the clock moves forward.
+   *
+   * @param ticksAdvanced The amount of ticks that the clock moved forward by.
+   */
   protected abstract void onTickAdvance(long ticksAdvanced);
 
+  /** Callback method for when the clock tick was reset to 0. */
   protected abstract void onTickReset();
 }
