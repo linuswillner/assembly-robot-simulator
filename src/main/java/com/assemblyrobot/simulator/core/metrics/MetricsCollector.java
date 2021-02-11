4189ee8cfe5b0849bb7cb5392f3d827459de30fb
Linus Willner <hello@linuswillner.me>
2021-02-11 18:04:25 +0200

    Document simulator.core.metrics package and add missing changes

--- src/main/java/com/assemblyrobot/simulator/core/metrics/MetricsCollector.java
+++ src/main/java/com/assemblyrobot/simulator/core/metrics/MetricsCollector.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import lombok.Getter;
 import lombok.NonNull;
 
+/**
+ * Generic metrics collector. Can be attached to any class to store metrics about various facets of
+ * said class.
+ */
 public class MetricsCollector {
   @Getter private final HashMap<String, Double> metrics = new HashMap<>();
   @Getter @NonNull private final MetricsCollectorType type;
@@ -13,14 +17,34 @@ public class MetricsCollector {
     CentralMetricsCollector.getInstance().registerMetricsCollector(collectingClassName, this);
   }
 
+  /**
+   * Get a metric by name.
+   *
+   * @param metricName Metric name to get.
+   * @return {@link Double} (Null if no metric by this name exists)
+   */
   public double getMetric(@NonNull String metricName) {
     return metrics.get(metricName);
   }
 
+  /**
+   * Get a metric by name, specifying a default value to use when a metric by this name does not
+   * exist.
+   *
+   * @param metricName Metric name to get.
+   * @param defaultValue Default value to use in case no metric by this name exists.
+   * @return {@link Double}
+   */
   public double getMetricWithDefault(@NonNull String metricName, double defaultValue) {
     return metrics.getOrDefault(metricName, defaultValue);
   }
 
+  /**
+   * Set or update a metric.
+   *
+   * @param metricName Metric name to set/update.
+   * @param measurement Metric value to set/update.
+   */
   public void putMetric(@NonNull String metricName, double measurement) {
     metrics.put(metricName, measurement);
   }
