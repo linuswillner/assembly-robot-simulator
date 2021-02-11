df3850e5935aa5e1a2f319c7ae1382229f7f09ea
Linus Willner <hello@linuswillner.me>
2021-02-11 20:50:09 +0200

    Fix redundantly verbose imports for EventQueueTest

--- src/test/java/com/assemblyrobot/simulator/core/events/EventQueueTest.java
+++ src/test/java/com/assemblyrobot/simulator/core/events/EventQueueTest.java
@@ -1,8 +1,6 @@
 package com.assemblyrobot.simulator.core.events;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import lombok.val;
