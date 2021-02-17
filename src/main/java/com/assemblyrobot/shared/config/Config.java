cafd01a983dbf534e8a0d5f2717fb8d5840f4e63
Linus Willner <hello@linuswillner.me>
2021-02-17 11:31:12 +0200

    Config class docs

--- src/main/java/com/assemblyrobot/shared/config/Config.java
+++ src/main/java/com/assemblyrobot/shared/config/Config.java
@@ -10,10 +10,16 @@ import lombok.val;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/** Config loader and manager class. */
 public abstract class Config {
   private static final Logger logger = LogManager.getLogger();
   private static ApplicationConfig config;
 
+  /**
+   * Returns the configuration as defined in resources/config/config.json.
+   *
+   * @return {@link ApplicationConfig}
+   */
   public static ApplicationConfig getConfig() {
     if (config == null) {
       load();
@@ -22,6 +28,12 @@ public abstract class Config {
     return config;
   }
 
+  /**
+   * (Re)loads the configuration file from resources/config/config.json.
+   *
+   * <p>An IOException in this method will cause the program to exit with code 1, as the config must
+   * be loaded in order for the application to function.
+   */
   @SneakyThrows
   public static void load() {
     val configFile = Config.class.getResource("/config/config.json");
