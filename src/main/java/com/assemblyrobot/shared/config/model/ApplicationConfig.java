2df4b58997613bdbe81054e91fa6e5221dd404a2
Linus Willner <hello@linuswillner.me>
2021-02-17 11:30:53 +0200

    Prevent unintended instantiation of config model classes

--- src/main/java/com/assemblyrobot/shared/config/model/ApplicationConfig.java
+++ src/main/java/com/assemblyrobot/shared/config/model/ApplicationConfig.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-public class ApplicationConfig {
+public abstract class ApplicationConfig {
   @Getter @Setter private NormalDistributionConfig arrivalIntervalParams;
   @Getter @Setter private NormalDistributionConfig assemblyTimeParams;
 }
