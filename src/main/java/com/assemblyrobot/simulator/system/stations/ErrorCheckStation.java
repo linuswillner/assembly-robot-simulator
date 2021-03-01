d8a5d4a29c261d2bb746ce9c1b03cdd4cc9d149f
Linus Willner <hello@linuswillner.me>
2021-03-01 18:41:52 +0200

    Fix a comment in the wrong place

--- src/main/java/com/assemblyrobot/simulator/system/stations/ErrorCheckStation.java
+++ src/main/java/com/assemblyrobot/simulator/system/stations/ErrorCheckStation.java
@@ -3,14 +3,8 @@ package com.assemblyrobot.simulator.system.stations;
 import com.assemblyrobot.simulator.core.generators.ErrorCheckTimeGenerator;
 import com.assemblyrobot.simulator.system.components.Station;
 import com.assemblyrobot.simulator.system.stages.ErrorCheckStage;
-import eduni.distributions.Normal;
 import lombok.NonNull;
 
-/**
- * Error checking duration generator. Generates arbitrary durations for error checking (measured in
- * seconds) during the Assembly stage based on a {@link Normal} distribution, with defaults
- * specified by the system and alterable by the user.
- */
 public class ErrorCheckStation extends Station implements Comparable<ErrorCheckStation> {
   public ErrorCheckStation(@NonNull ErrorCheckStage stage) {
     super(stage.getStageController());
