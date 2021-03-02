9adab869a08e17318c9ca132437950289d23bc7e
Linus Willner <hello@linuswillner.me>
2021-03-02 22:18:41 +0200

    Run tests according to default config

--- src/test/java/com/assemblyrobot/simulator/core/generators/ArrivalEventIntervalGeneratorTest.java
+++ src/test/java/com/assemblyrobot/simulator/core/generators/ArrivalEventIntervalGeneratorTest.java
@@ -2,6 +2,9 @@ package com.assemblyrobot.simulator.core.generators;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.assemblyrobot.shared.config.Config;
+import com.assemblyrobot.shared.config.model.NormalDistributionConfig;
+import com.assemblyrobot.utils.RandomUtils;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 
@@ -9,21 +12,23 @@ class ArrivalEventIntervalGeneratorTest {
   private final ArrivalEventIntervalGenerator generator =
       ArrivalEventIntervalGenerator.getInstance();
 
+  private final NormalDistributionConfig config = Config.getConfig().getAssemblyTimeParams();
+
   @Test
   void nextInt() {
     val result = generator.nextInt();
-    assertTrue(result >= 15 && result <= 25);
+    assertTrue(RandomUtils.resultIsWithinVariance(result, config.getMean(), config.getVariance()));
   }
 
   @Test
   void nextLong() {
     val result = generator.nextLong();
-    assertTrue(result >= 15L && result <= 25L);
+    assertTrue(RandomUtils.resultIsWithinVariance(result, config.getMean(), config.getVariance()));
   }
 
   @Test
   void nextDouble() {
     val result = generator.nextDouble();
-    assertTrue(result >= 15.0 && result <= 25.0);
+    assertTrue(RandomUtils.resultIsWithinVariance(result, config.getMean(), config.getVariance()));
   }
 }
