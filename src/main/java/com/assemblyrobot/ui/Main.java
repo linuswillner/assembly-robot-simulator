80c6371ef4a628ee5464a8c0cfffb0ecefa73704
Anna Smati <anna.smati@gmail.com>
2021-03-08 17:36:58 +0200

    Change stage size to 1024x740

--- src/main/java/com/assemblyrobot/ui/Main.java
+++ src/main/java/com/assemblyrobot/ui/Main.java
@@ -5,14 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.val;
 
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent rootScene = FXMLLoader.load(getClass().getResource("/scenes/Main.fxml"));
-    primaryStage.setTitle("Hello World");
+    Parent rootScene = FXMLLoader.load(getClass().getResource("/scenes/MainWindow.fxml"));
+    primaryStage.setTitle("Assembly Robot Simulator");
     primaryStage.setScene(new Scene(rootScene));
     primaryStage.show();
+
+    val width = 1024;
+    val height = 740;
+    primaryStage.setMinWidth(width);
+    primaryStage.setMaxWidth(width);
+    primaryStage.setMinHeight(height);
+    primaryStage.setMaxHeight(height);
   }
 
   public static void main(String[] args) {
