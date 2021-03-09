c16064d94e5e529b993158b55455a54bf3dba71a
Linus Willner <hello@linuswillner.me>
2021-03-09 23:55:39 +0200

    Fill in options on boot

--- src/main/java/com/assemblyrobot/ui/views/OptionsEditor.java
+++ src/main/java/com/assemblyrobot/ui/views/OptionsEditor.java
@@ -1,5 +1,7 @@
 package com.assemblyrobot.ui.views;
 
+import com.assemblyrobot.shared.config.Config;
+import com.assemblyrobot.shared.config.model.NormalDistributionConfig;
 import com.assemblyrobot.ui.utils.TextFieldUtils;
 import java.net.URL;
 import java.util.Arrays;
@@ -8,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import lombok.val;
 
 public class OptionsEditor implements Initializable {
   // Root container
@@ -41,19 +44,68 @@ public class OptionsEditor implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    // Configure integer-only TextFields
     Arrays.stream(TextFieldUtils.getAllTextFields(settingsRoot))
-        .forEach(
-            textField -> {
-              // Require numeric values for all fields
-              textField
-                  .textProperty()
-                  .addListener(
-                      (observable, oldValue, newValue) -> {
-                        if (!newValue.matches("\\d*")) {
-                          textField.setText(newValue.replaceAll("[^\\d]", ""));
-                        }
-                      });
-            });
+        .filter(textField -> !textField.getId().equals("errorOccurrenceCoefficient"))
+        .forEach(TextFieldUtils::configureIntegerOnlyTextField);
+
+    // Configure the errorOccurrenceCoefficient field to accept decimals
+    TextFieldUtils.configureDecimalOnlyTextField(errorOccurrenceCoefficient);
+
+    // Populate configuration into the fields
+    val config = Config.getConfig();
+
+    // Simulation settings
+
+    // Arrival interval
+    setMeanAndVariance(
+        arrivalIntervalAverage, arrivalIntervalVariance, config.getArrivalIntervalParams());
+
+    // Assembly time
+    setMeanAndVariance(assemblyTimeAverage, assemblyTimeVariance, config.getAssemblyTimeParams());
+
+    // Error check time
+    setMeanAndVariance(
+        errorCheckTimeAverage, errorCheckTimeVariance, config.getErrorCheckTimeParams());
+
+    // Error occurrences
+    errorOccurrenceCoefficient.setText(
+        Double.toString(config.getErrorOccurrenceParams().getTypeDistributionMean()));
+    errorOccurrenceThreshold.setText(
+        Integer.toString(config.getErrorOccurrenceParams().getErrorThreshold()));
+
+    // Station amounts
+
+    val stationParams = config.getStationParams();
+    setStationAmount(assemblyStationAmount, stationParams.getAssemblyStationAmount());
+    setStationAmount(errorCheckStationAmount, stationParams.getErrorCheckStationAmount());
+    setStationAmount(fittingStationAmount, stationParams.getFittingFixStationAmount());
+    setStationAmount(boltingStationAmount, stationParams.getBoltingFixStationAmount());
+    setStationAmount(rivetingStationAmount, stationParams.getRivetingFixStationAmount());
+    setStationAmount(weldingStationAmount, stationParams.getWeldingFixStationAmount());
+    setStationAmount(positionStationAmount, stationParams.getReturningFixStationAmount());
+
+    // Error fix times
+    val fixTimes = config.getErrorFixTimes();
+    setFixTime(fittingFixTime, fixTimes.getFittingFixTime());
+    setFixTime(boltingFixTime, fixTimes.getBoltingFixTime());
+    setFixTime(rivetingFixTime, fixTimes.getRivetingFixTime());
+    setFixTime(weldingFixTime, fixTimes.getWeldingFixTime());
+    setFixTime(positionFixTime, fixTimes.getReturningFixTime());
+  }
+
+  private void setMeanAndVariance(
+      TextField meanField, TextField varianceField, NormalDistributionConfig config) {
+    meanField.setText(Integer.toString(config.getMean()));
+    varianceField.setText(Integer.toString(config.getVariance()));
+  }
+
+  private void setStationAmount(TextField amountField, int stationAmount) {
+    amountField.setText(Integer.toString(stationAmount));
+  }
+
+  private void setFixTime(TextField fixTimeField, int time) {
+    fixTimeField.setText(Integer.toString(time));
   }
 
   @FXML
