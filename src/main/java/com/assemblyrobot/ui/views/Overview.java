ae038ff0a466086aa6f7e73b1963d9c5baf5d1fc
Anna Smati <anna.smati@gmail.com>
2021-03-10 02:48:18 +0200

    QuickFix: main controls

--- src/main/java/com/assemblyrobot/ui/views/Overview.java
+++ src/main/java/com/assemblyrobot/ui/views/Overview.java
@@ -9,7 +9,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Overview implements Initializable, View {
 
@@ -17,17 +20,13 @@ public class Overview implements Initializable, View {
   private final OverviewController controller = new OverviewController();
   private boolean hasStarted = false;
   private boolean isPause = false;
+  private static final Logger logger = LogManager.getLogger();
 
   @FXML private Button buttonStatus;
   @FXML private Slider sliderSpeed;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
-
-  @Override
-  public void setMain(Main main) {
-    this.main = main;
-  }
 
   @FXML
   public void openOptionsEditor(ActionEvent actionEvent) {
@@ -44,4 +43,38 @@ public class Overview implements Initializable, View {
 
   @FXML
   public void openAbout(ActionEvent actionEvent) {}
+
+  @FXML
+  public void controlSimulation(ActionEvent actionEvent) {
+    if (!hasStarted) {
+      buttonStatus.setText("Pause");
+      controller.startEngine();
+      hasStarted = true;
+    } else {
+      if (!isPause) {
+        buttonStatus.setText("Resume");
+        isPause = true;
+        controller.setPause(true);
+        controller.setCanProceed(false);
+      } else {
+        buttonStatus.setText("Pause");
+        isPause = false;
+        controller.setPause(false);
+        controller.setCanProceed(true);
+        logger.trace("Simulation resumed.");
+      }
+    }
+  }
+
+  @FXML
+  public void takeStep(ActionEvent actionEvent) {
+    controller.takeStep();
+  }
+
+  @FXML
+  public void onSliderChanged(MouseEvent mouseEvent) {
+    double value = sliderSpeed.getValue();
+    controller.setSpeed(value);
+    System.out.println("Slider speed changed to x" + value);
+  }
 }
